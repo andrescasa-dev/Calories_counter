9f6b808c8b46b55607f62d7f1653d58142fbb4a4
andrescasa-dev <andrescasa.dev@gmail.com>
2022-04-18 09:25:39 -0500

    save in memory the registers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const btn_submit = document.getElementById('submit');
 const table = document.getElementById('counter__table');
 const form = document.getElementById('form');
-const inputs_list = form.querySelectorAll('input[class = "field"]');
+const inputs_list = Array.from(form.querySelectorAll('input[class = "field"]'));
+const register = [];
+
+
 
 function emptyFields(){
   let isEnyEmpty = false
@@ -17,36 +20,73 @@ function emptyFields(){
   return isEnyEmpty;
 }
 
-function addElement(){
-  const fragment = new DocumentFragment();
-  inputs_list.forEach( 
-    field =>{
-      let lbl = document.createElement('tag');
-      lbl.innerText = field.value;
-      field.value = '';
-      fragment.appendChild(lbl)
-    }
-  );
-  
-  table.appendChild(fragment);
+function addRegister(){
+  const newResgister = {
+    description: inputs_list[0].value,
+    calories: numericInput(inputs_list[1].value),
+    carbs: numericInput(inputs_list[2].value),
+    protein: numericInput(inputs_list[3].value)
+  }
+  register.push(newResgister);
 }
 
-function manageField(event){
-  debugger;
-  event.preventDefault();//The event "click" ocurred on a btn inside a from, the web try to do a get (http method), so we need to prevent it.
-  if(!emptyFields()){
-    addElement();  
+function numericInput(value){
+  let parseValue = parseInt(value);
+  if(isNaN(parseValue)){
+    throw Error ('Incorrect input values, number required')
+  }
+  else{
+    return parseValue
   }
 }
 
+function showRegister(){
+  const fragment = new DocumentFragment();
+  let i = 0;
+  inputs_list.forEach( 
+    input =>{
+      createHTMLElement(fragment, input);
+      resetInputValue(i++);
+    }
+  );
+  table.appendChild(fragment);
+}
+
+function createHTMLElement(fragment, input){
+  let lbl = document.createElement('tag');
+  lbl.innerText = input.value;
+  fragment.appendChild(lbl)
+}
+
+function resetInputValue(index){
+  inputs_list[index].value = '';
+}
+
+function manageField(event){
+  try {
+    event.preventDefault();//The event "click" ocurred on a btn inside a from, the web try to do a get (http method), so we need to prevent it.
+    if(!emptyFields()){
+      addRegister();
+      console.log(register);
+      showRegister();  
+    }
+  } catch (error) {
+    alert(error)
+  }
+  
+}
+
+function getValueInputs(){
+  const values = inputs_list.map(input => input.value);
+  return values;
+}
+
 function toggleEmptyClass(event){
-  debugger;
   const input = event.target;
   if(input.tagName === 'INPUT'){
     input.classList.remove('field--empty');
   }
 }
-
 
 form.addEventListener('keydown', toggleEmptyClass)
 btn_submit.addEventListener('click', manageField)
